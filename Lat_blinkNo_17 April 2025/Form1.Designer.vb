658cf499a89f4bfcef3fc9189016a721c25d37b9
Nek0ding <johndoe@example.com>
2025-04-15 20:40:20 +0700

    Add project files.

---
+++ Lat_blinkNo_17 April 2025/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        btn_no = New CheckBox()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(300, 70)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(344, 48)
+        Label1.TabIndex = 0
+        Label1.Text = "Are you handsome ?"
+        ' 
+        ' btn_no
+        ' 
+        btn_no.AutoSize = True
+        btn_no.Font = New Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btn_no.Location = New Point(414, 256)
+        btn_no.Name = "btn_no"
+        btn_no.Size = New Size(94, 49)
+        btn_no.TabIndex = 1
+        btn_no.Text = "NO"
+        btn_no.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.tak_tau
+        PictureBox1.Location = New Point(26, 139)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(274, 357)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(878, 544)
+        Controls.Add(btn_no)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btn_no As CheckBox
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Lat_blinkNo_17 April 2025/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub cmb_no(sender As Object, e As MouseEventArgs) Handles btn_no.MouseMove
+        Dim koor_horizontal = Int(Rnd() * (Me.Width - 100 - btn_no.Width))
+        Dim koor_vertikal = Int(Rnd() * (Me.Height - 100 - btn_no.Height))
+        btn_no.Left = koor_horizontal
+        btn_no.Top = koor_vertikal
+        MsgBox(koor_horizontal)
+        MsgBox(koor_vertikal)
+    End Sub
+End Class
